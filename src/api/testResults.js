3ac91ab69673f4b9f0519c481c4fc4c997a1486d
ahh0619 <ahh900619@gmail.com>
2024-11-26 14:48:06 +0900

    fix:server.js 수정

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/testResults";
+const API_URL = "https://your-json-server.vercel.app/testResults";
 
 // 테스트 결과를 가져오는 함수
 export const getTestResults = async () => {
